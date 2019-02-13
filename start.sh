d5120c057c393a74e8ca4eec5ec30ddbe54c8e36
jasonlin <jasonlin@elevenbuckets.com>
2019-02-13 09:54:11 -0700

    adding options for geth to apply netrestrict or to expose RPC to host

--- start.sh
+++ start.sh
@@ -1,23 +1,26 @@
 #!/bin/bash -l
 
 APPDIR='/data/11be'
-export rpchost=`hostname -I`
+export rpchost=`hostname -I`; # for both geth and 11be rpc services
+
+optCIDR=''
+optRPC=''
 
 if [ "${NETRESTRICT}x" != "x" ]; then
-( geth --rinkeby \
-	--syncmode light \
-	--datadir $DATADIR \
-	--rpc --rpcapi "eth,net" \
-	--netrestrict $NETRESTRICT \
-	--nat none \
-	--rpccorsdomain "*" & );
-else
-( geth --rinkeby \
-	--syncmode light \
-	--datadir $DATADIR \
-	--rpc --rpcapi "eth,net" \
-	--nat none \
-	--rpccorsdomain "*" & );
+	optCIDR="--netrestrict ${NETRESTRICT}"
 fi
+
+if [ "${EXPOSERPC}x" != "x" ]; then
+	optRPC="--rpcaddr ${rpchost}"
+fi
+
+( geth --rinkeby \
+	--syncmode light \
+	--datadir $DATADIR \
+	--rpc --rpcapi "eth,net" \
+        $optRPC \
+	$optCIDR \
+	--nat none \
+	--rpccorsdomain "*" & );
 
 cd $APPDIR && sleep 7 && $APPDIR/node_modules/.bin/bladecli 
